bf2135ecb5522bcd2520249a6b8397e58264dc66
Alena Konovalova <konovalova.alena@gmail.com>
2020-12-19 17:53:15 +0300

    add User model

--- take_it_easy_app/models.py
+++ take_it_easy_app/models.py
@@ -3,3 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 # TODO set delete behavior
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(
+        db.String(50),
+        unique=True, index=True, nullable=False)
+    password = db.Column(
+        db.String(128),
+        nullable=False)
+    userpic_url = db.Column(
+        db.String(),
+        nullable=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
