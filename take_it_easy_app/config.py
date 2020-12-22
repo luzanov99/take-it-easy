6f2513841f03fe0753d240257e6be12212dd2439
Konovalova Alena <konovalova.alena@gmail.com>
2020-12-22 13:18:45 +0300

    change app name

--- take_it_easy_app/config.py
+++ take_it_easy_app/config.py
@@ -2,5 +2,5 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-    basedir, "..", "webapp.db"
+    basedir, "..", "take_it_easy_app.db"
 )
