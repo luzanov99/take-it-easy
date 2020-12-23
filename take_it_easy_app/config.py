f49a5713a6e5236e9fbc9c653302c1a1c62f9450
Alena Konovalova <konovalova.alena@gmail.com>
2020-12-23 18:59:15 +0300

    turn off sqlalchemy deprecation warning

--- take_it_easy_app/config.py
+++ take_it_easy_app/config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
     basedir, "..", "take_it_easy_app.db"
 )
+SQLALCHEMY_TRACK_MODIFICATIONS = False
