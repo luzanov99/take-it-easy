4848fdd3fce69ef4d4ee6c09e2ab2ef93fb76dd2
Luazanov Yevgen <luzanov.zena@gmail.com>
2020-12-24 18:52:51 +0200

    Rename folder on webapp and adding logging form

--- create_db.py
+++ create_db.py
@@ -1,3 +1,3 @@
-from take_it_easy_app import db, create_app
+from webapp import db, create_app
 
 db.create_all(app=create_app())
--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 tags = db.Table(
@@ -9,7 +10,7 @@ tags = db.Table(
 )
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(
         db.String(50), unique=True, index=True, nullable=False
@@ -17,6 +18,18 @@ class User(db.Model):
     password = db.Column(db.String(128), nullable=False)
     userpic_url = db.Column(db.String(), nullable=True)
     comment = db.relationship("Comment", backref=db.backref("user"), lazy=True)
+    role =db.Column(db.String(10), index=True)
+    
+    def set_password(self, password):
+        self.password=generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        
+        return self.role =='admin'
 
     def __repr__(self):
         return f"<User {self.username}>"
