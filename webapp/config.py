7afe744e81a697c0839594628f09038b7809d4e9
Alena Konovalova <konovalova.alena@gmail.com>
2020-12-29 00:04:11 +0300

    turn off SQLAlchemy deprecation warning

--- webapp/config.py
+++ webapp/config.py
@@ -1,7 +1,10 @@
 import os
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
     basedir, "..", "take_it_easy_app.db"
 )
-SECRET_KEY="12321asdwfq!$@123"
+
+SECRET_KEY = "12321asdwfq!$@123"
